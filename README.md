47fbb13a3d01c2995032157c8d20eae5596be1bc
Molly Kan <molly.kan@evergreensystemltd.com>
2018-06-06 21:25:26 +0800

    First Upload

---
+++ README.md
@@ -0,0 +1,25 @@
+# Spotify-web-widget
+
+Get the Spotify Web Widget.
+
+[https://mollykannn.github.io/Spotify-web-widget](https://mollykannn.github.io/Spotify-web-widget)
+![Screenshot](https://mollykannn.github.io/Spotify-web-widget/screenshot.png)
+
+
+## 安裝 (Install)
+
+```shell
+npm install
+```
+
+## 使用方法 (Usage)
+
+建立檔案 (Create files)
+```shell
+npm run build
+```
+
+運行 (Run)
+```shell
+npm run serve
+```
---
+++ src/views/Vertify.vue
@@ -0,0 +1,25 @@
+<script>
+var hash = {}
+window.location.hash
+  .replace(/^#\/?/, '')
+  .split('&')
+  .forEach(function (kv) {
+    var spl = kv.indexOf('=')
+    if (spl !== -1) {
+      hash[kv.substring(0, spl)] = decodeURIComponent(kv.substring(spl + 1))
+    }
+  })
+if (hash.access_token) {
+  window.opener.postMessage(
+    JSON.stringify({
+      type: 'access_token',
+      access_token: hash.access_token,
+      expires_in: hash.expires_in || 0
+    }),
+    '*'
+  )
+  window.close()
+}
+export default {
+}
+</script>
