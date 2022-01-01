37b30af6337f8572905dc45f31bcd350ea4ead28
Molly Kan <molly.kan@evergreensystemltd.com>
2022-01-01 22:48:09 +0800

    edit eslint & stylelint

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,14 +1,7 @@
 module.exports = {
   root: true,
-  parserOptions: {
-    sourceType: 'module',
-  },
-  parser: 'vue-eslint-parser',
-
   env: {
-    browser: true,
-    node: true,
-    es6: true,
+    node: true
   },
 
   rules: {
--- vite.config.js
+++ vite.config.js
@@ -2,11 +2,12 @@ import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import { VitePWA } from 'vite-plugin-pwa'
 import eslintPlugin from 'vite-plugin-eslint'
+import viteStylelint from '@amatlash/vite-plugin-stylelint'
 import path from 'path'
 
 export default defineConfig({
   base: '/spotify-web-widget',
-  plugins: [vue(), eslintPlugin(), VitePWA({
+  plugins: [vue(), viteStylelint(), eslintPlugin(), VitePWA({
     mode: 'development',
     srcDir: 'src',
     filename: 'sw.js',
